88fbf88351ca4b39dfbc07c0b855a5d7c7d19896
maqsoodahmad-1 <maqsodmaxood75@gmail.com>
2022-07-30 06:39:49 +0530

    learning mongo

---
+++ Express/myApp/model/usermodel.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const userSchema = new mongoose.Schema ({
+   first_name: { type: String, default: null },
+   last_name: { type: String, default: null },
+   email: { type: String, unique: true },
+   password: { type: String },
+   token: { type: String },
+ });
+
+//Exporting 
+ //var userModel= mongoose.model("users", userSchema);
+
+module.exports = mongoose.model('Users', userSchema);
